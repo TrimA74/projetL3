cfda81253ed34466a7f7fc0a51ed9027a6d0d0aa
Curamide <keehlducrettet@hotmail.fr>
2017-02-11 10:42:09 +0100

    modife site

---
+++ Site/index.js
@@ -0,0 +1,15 @@
+function changeX (val) {
+	document.getElementById('NumberX').value = val;
+}
+
+function changeT (val) {
+	document.getElementById('NumberT').value = val;
+}
+
+function changeB (val) {
+	document.getElementById('NumberB').value = val;
+}
+
+function changeA (val) {
+	document.getElementById('NumberA').value = val;
+}
