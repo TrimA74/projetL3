01030134a1dedf2ba37fd99992bb38c040b92847
Damien Ducrettet <ducrettd@localhost>
2017-04-04 08:44:43 +0200

    affiche.php

---
+++ Test/siteDynamiqueAvecFichier/data/Heat conduction in insulated wall/zip_to_csv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for param in "$@"
+do
+	fullfilename=$(basename $param)
+	filename=${fullfilename%.*}
+	echo "-----------------------------"
+	echo "full zip name = $fullfilename"
+	echo "foldername = $filename"
+ 	unzip ./$param -d $filename
+	cd $filename/
+	sed -i -re 's/\t/,/g' *.txt | sed -i -re 's/( )//g' *.txt
+		for file in *.txt; do
+    		mv "$file" "`basename "$file" .txt`.csv"
+		done
+	cd ..
+	rm $param
+	echo "txt  to  csv execute"
+done
+
+
+
+
+
+
