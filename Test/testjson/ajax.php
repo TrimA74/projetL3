07e997475a23dbd41702a47653b356d5d700f587
TrimA <florianseb96@hotmail.fr>
2017-05-10 17:27:28 +0200

    ajout de la version avec le JSON

---
+++ Test/testjson/ajax.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+$cat = "Moisture diffusion in porous material";
+$set = "set1";
+$chemin = "./data/".$cat."/".$set."/";
+$explode = explode("\n", file_get_contents($chemin.'meta_donnees.csv'));
+$result = array();
+$compteurResult = 0;
+for($i=0; $i<sizeof($explode); $i++)
+{
+    $explode[$i] = trim($explode[$i]); //on eneleve les espaces en fin et debut pour bien detecter ensuite les lignes vides
+    if($explode[$i]!="")
+    {
+        $result[$compteurResult] = explode(",", $explode[$i]);
+        $compteurResult++;
+    }
+        
+}
+var_dump($result);*/
+
+
+if (isset($_POST['myFunction']) && $_POST['myFunction'] != '')
+{
+    $_POST['myFunction']($_POST);
+}
+
+
+function chargeSet($data)
+{
+    $cat = $data['myParams']['cat'];
+    $set = $data['myParams']['set'];
+    $chemin = "./data/".$cat."/".$set."/";
+    $explode = explode("\n", file_get_contents($chemin.'meta_donnees.csv'));
+    $result = array();
+    $compteurResult = 0;
+    for($i=0; $i<sizeof($explode); $i++)
+    {
+        $explode[$i] = trim($explode[$i]); //on eneleve les espaces en fin et debut pour bien detecter ensuite les lignes vides
+        if($explode[$i]!="")
+        {
+            $result[$compteurResult] = explode(",", $explode[$i]);
+            $compteurResult++;
+        }
+            
+    }
+    echo json_encode($result);
+    
+}
+
+function chargeJson($data)
+{
+    $cat = $data['myParams']['cat'];
+    $set = $data['myParams']['set'];
+    echo file_get_contents("./data/".$cat."/set".$set."/metadata.json");
+}
+
+
+function chargeMatrice($data)
+{
+    $cat = $data['myParams']['cat'];
+    $set = $data['myParams']['set'];
+    $nomMatrice = $data['myParams']['matrice'];
+    $chemin = "./data/".$cat."/".$set."/";
+    $result = file_get_contents($chemin.$nomMatrice.'.csv');
+    
+    //fait dans le js
+    /*for($i=0; $i<sizeof($result); $i++)
+    {
+        if($result[$i]!="")
+            $result[$i] = explode(",", $result[$i]);
+        else
+            unset($result[$i]);
+    }*/
+    echo json_encode($result);
+}
+
+
+    
+    
