7cfaeb3f0e6841184b14438d7b4b482315b9675c
olivier <olivier.sthioul@etu.univ-savoie.fr>
2017-05-17 14:55:01 +0200

    enlever le logo et son nom dans le footer + garder un footer minimum meme vide

--- Test/testjson/footer.php
+++ Test/testjson/footer.php
@@ -1,11 +1,3 @@
 <div class="footerDescription">
-	<div class="col-md-4 logo">
-		<img src="img/locie-logo.png" alt="" width="70px">
-	</div>
-	<div class="col-md-8">
-		<p>M. BERGER Julien</p>
-		<p>Chercheur au LOCIE</p>
-		<p>Docteur en Génie Civil et Sciences de l'Habitat</p>
-	</div>
 	
 </div>
